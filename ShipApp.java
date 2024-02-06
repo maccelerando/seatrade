72a72d28b4a4713841d712e19d2cd957c5e84c4f
maccelerando <marcel.guthmann@googlemail.com>
2024-02-06 14:09:43 +0100

    added method autoCompleteMethod to ShipApp; not used yet

--- ShipApp.java
+++ ShipApp.java
@@ -48,6 +48,19 @@ public class ShipApp {
       }
 
    }
+   
+   private static String autoCompleteInput(String input) {
+	   String completed = "";
+	   for (int i = 0; i < inputSelectionStrings.length; i++) {
+		   for (int j = 0; j < inputSelectionStrings[i].length(); j++) {
+			   if (input.equals(inputSelectionStrings[i].substring(0, j + 1))) {
+				   completed = inputSelectionStrings[i];
+				   break;
+			   }
+		   }
+	   }
+	   return completed;
+   }
 
    public static String setShipName() {
       String shipName = DEFAULT_SHIP_NAMES[0];
