cbda6384977ecb56435c3d9bf786f7ea51cc7e03
Marcel Guthmann <m.guthmann@glp.de>
2024-02-13 10:36:06 +0100

    renamed file Connect.java

--- Connect.java
+++ Connect.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class connect {
+public class Connect {
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
