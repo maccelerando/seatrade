853dbb298850f0190df8181c08a5b7b8e13c1c9a
Marcel Guthmann <m.guthmann@glp.de>
2024-02-16 12:59:11 +0100

    experiment with color in command line

--- CompanyServer.java
+++ CompanyServer.java
@@ -9,13 +9,15 @@ public class CompanyServer extends Thread {
   private ServerSocket serverSocket;
   private HashMap<String, Socket> shipAppSockets;
   private boolean exit;
+  public static final String ANSI_PURPLE = "\u001B[35m";
+  public static final String ANSI_RESET = "\u001B[0m";
 
   public CompanyServer(int portNumber) {
     this.portNumber = portNumber;
     try {
       this.serverSocket = new ServerSocket(portNumber);
     } catch (IOException e) {
-      System.out.println("Error: server socket not created. " + e.getMessage());
+      printlnPurple("Error: server socket not created. " + e.getMessage());
     }
     this.shipAppSockets = new HashMap<String, Socket>();
     this.exit = true;
@@ -23,14 +25,19 @@ public class CompanyServer extends Thread {
 
   @Override
   public void run() {
-    System.out.println("Server is waiting for client connection on port " + portNumber);
+    printlnPurple("Server is waiting for client connection on port " + portNumber);
+
     try {
       shipAppSockets.put("socket1", serverSocket.accept());
     } catch (IOException e) {
-      System.out.println("Error: socket1 not put " + e.getMessage());
+      printlnPurple("Error: socket1 not put " + e.getMessage());
     }
-    System.out.println(shipAppSockets.get("socket1"));
+    printlnPurple(shipAppSockets.get("socket1").toString());
 
+  }
+
+  public void printlnPurple(String s) {
+    System.out.println(ANSI_PURPLE + s + ANSI_RESET);
   }
 
   // TODO Socket clientSocket = serverSocket.accept() method
